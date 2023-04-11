f7c00c9b79c927bb10dc400376519f8f2bbf31fd
Alen Redek <alen@redek.me>
2023-04-11 10:12:50 +0200

    Multiplication side effects

--- src/money/dollar.spec.ts
+++ src/money/dollar.spec.ts
@@ -3,7 +3,10 @@ import { Dollar } from './dollar';
 describe('Dollar', () => {
   it('should multiply', () => {
     const five: Dollar = new Dollar(5);
-    five.times(2);
-    expect(five.amount).toEqual(10);
+    let product: Dollar = five.times(2);
+    expect(product.amount).toEqual(10);
+    product = five.times(3);
+    expect(product.amount).toEqual(15);
+  });
   });
 });

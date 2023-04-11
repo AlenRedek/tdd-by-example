938377bc110a3728f39b49c566eb8d91caa6074f
Alen Redek <alen@redek.me>
2023-04-11 10:12:50 +0200

    Added equality test for different currencies

--- src/money/money.ts
+++ src/money/money.ts
@@ -1,7 +1,10 @@
 export class Money {
   protected amount: number;
 
-  public equals(money: Money): boolean {
-    return this.amount === money.amount;
+  public equals(money: any): boolean {
+    return (
+      this.amount === money.amount &&
+      this.constructor.name === money.constructor.name
+    );
   }
 }

6c38b3301359457f14ecf1d30f1fb3fc7ef13c71
Alen Redek <alen@redek.me>
2023-04-11 10:12:50 +0200

    Changed property amount to private

--- src/money/dollar.spec.ts
+++ src/money/dollar.spec.ts
@@ -3,13 +3,11 @@ import { Dollar } from './dollar';
 describe('Dollar', () => {
   it('should multiply', () => {
     const five: Dollar = new Dollar(5);
-    let product: Dollar = five.times(2);
-    expect(product.amount).toEqual(10);
-    product = five.times(3);
-    expect(product.amount).toEqual(15);
+    expect(new Dollar(10)).toEqual(five.times(2));
+    expect(new Dollar(15)).toEqual(five.times(3));
   });
 
-  it('should equals', () => {
+  it('should check for equality', () => {
     expect(new Dollar(5).equals(new Dollar(5))).toBeTruthy();
     expect(new Dollar(5).equals(new Dollar(6))).toBeFalsy();
   });
